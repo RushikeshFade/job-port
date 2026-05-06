2544fa8f6ac59349e3ca71f0be5e8667a9cf11b7
RushikeshFade <rushifade1212@gmail.com>
2026-05-06 21:00:02 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,5 @@
 # Install dependencies and set up the environment for the project
 # Check if the script is run as root
 
-pip install -r requirements.txt
+pip install -r requirements.txt/requirements.txt
 echo "All dependencies have been installed successfully."
