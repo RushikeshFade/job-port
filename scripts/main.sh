f1aff9452c50d60de0de19cf601e7685930036d9
rushi <rushifade1212@gmail.com>
2026-05-07 22:17:52 +0530

    solve error

---
+++ scripts/main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+echo "start the script."
+
+pwd
+
+echo"list the file"
+ls -l
+
+echo"create fiel main2.txt"
+touch  main3.txt
+echo "hello" >> main2.txt
+
+echo"list the new files"
+ls -l
+
+
