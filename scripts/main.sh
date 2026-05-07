b32e792a6d5a4fa91381c61ec30da46f57be755c
rushi <rushifade1212@gmail.com>
2026-05-07 22:24:01 +0530

    solve error

--- scripts/main.sh
+++ scripts/main.sh
@@ -8,8 +8,8 @@ echo"list the file"
 ls -l
 
 echo"create fiel main2.txt"
-touch  main3.txt
-echo "hello" >> main2.txt
+touch  main.txt
+echo "hello" >> /scripts/main.txt
 
 echo"list the new files"
 ls -l
